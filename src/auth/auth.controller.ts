3e3fa542385b8fb9a25435f2ff86e6f4a35a2ea3
BACNINH-VN\V0518330 <lequangdung451993@gmail.com>
2026-08-22 14:11:06 +0700

    Add Swagger ApiBody for login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,29 @@
 import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiTags, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth') // Tiền tố: http://localhost:3000/auth
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('login') // Kết hợp thành API: POST /auth/login
-  async login(@Body() body: any) {
-    return this.authService.login(body.email, body.password);
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: { type: 'string', example: 'admin2026@gmail.com' },
+        password: { type: 'string', example: '123' },
+      },
+      required: ['email', 'password'],
+    },
+  })
+  // async login(@Body() body: any) {
+  //   return this.authService.login(body.email, body.password);
+  // }
+  async login(@Body() body: { email: string; pass: string }) {
+    // Lưu ý: Kiểm tra key gửi từ Frontend/Swagger truyền đúng vào tham số
+    return this.authService.login(body.email, body.pass || body['password']);
   }
 }
